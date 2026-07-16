97b975fb7527d1b3eb9a8ef5c61343c6667c61d0
Priyam <priyammondal2nd@gmail.com>
2026-07-17 03:33:06 +0530

    perf: add origin parameter to YouTube iframe to prevent cross-origin handshake latency

--- study-tracker/frontend/src/pages/SubjectPage.jsx
+++ study-tracker/frontend/src/pages/SubjectPage.jsx
@@ -504,7 +504,7 @@ export default function SubjectPage() {
               >
                 <iframe
                   id="yt-player-iframe"
-                  src={`https://www.youtube.com/embed/${activeLecture.video_id}?autoplay=1&rel=0&enablejsapi=1`}
+                  src={`https://www.youtube.com/embed/${activeLecture.video_id}?autoplay=1&rel=0&enablejsapi=1&origin=${encodeURIComponent(window.location.origin)}`}
                   style={{ position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', border: 0 }}
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                   allowFullScreen
